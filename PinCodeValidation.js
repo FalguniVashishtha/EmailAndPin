c75d2f9efbfcb5e76f0adebe13921787bd0f5d90
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-09-26 23:18:03 +0530

    check pincode validation

---
+++ PinCodeValidation.js
@@ -0,0 +1,24 @@
+console.log("Welcome To PinCode Validation: ");
+const pincodeRegex = RegExp("^[1-9][0-9]{5}$");
+
+function validatePincode(pincode){
+    if(pincodeRegex.test(pincode))
+        console.log(pincode + " : Valid PinCode!");
+    else
+        throw pincode + " : Invalid Pincode!";
+}
+
+try{
+    validatePincode("400088");
+    validatePincode("004456");   
+}
+catch(e){
+    console.error(e);
+}
+
+try{
+    validatePincode("400");  
+}
+catch(e){
+    console.error(e);
+}
